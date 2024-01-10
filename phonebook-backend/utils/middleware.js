ee14503f84864eba5159989acd9180f9632815db
josuecross <josue12cruz34@gmail.com>
2024-01-09 21:28:59 -0600

    create exports for middleware

---
+++ phonebook-backend/utils/middleware.js
@@ -0,0 +1,45 @@
+const morgan = require('morgan')
+const logger = require('./logger')
+
+const requestLoggerMorgan = morgan(function (tokens, req, res) {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    req.method === 'POST' ? JSON.stringify(req.body) : ''
+  ].join(' ')
+})
+
+const requestLogger = (request, response, next) => {
+  logger.info('Method:', request.method)
+  logger.info('Path:  ', request.path)
+  logger.info('Body:  ', request.body)
+  logger.info('---')
+  next()
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+module.exports = {
+  requestLoggerMorgan,
+  unknownEndpoint,
+  errorHandler,
+  requestLogger
+}
